26f5580f037db1ebeeae08489ddb31825484755a
Sam Douglas <44397135+Sam-Douglas@users.noreply.github.com>
2026-06-11 18:01:44 +0100

    Add project files.

---
+++ ShowPathOnTabs.Core/Captioning/TabCaptionBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShowPathOnTabs.Core.Captioning
+{
+    public static class TabCaptionBuilder
+    {
+        public static string Build(
+            string tabPath,
+            IReadOnlyList<string> openTabPaths)
+        {
+            return tabPath;
+        }
+
+        private static string AddPathToCaption(
+            string tabPath,
+            IReadOnlyList<string> otherOpenTabPaths)
+        {
+            return tabPath;
+        }
+    }
+}
---
+++ ShowPathOnTabs.UnitTests/Captioning/TabCaptionBuilderTests.cs
@@ -0,0 +1,26 @@
+﻿using static ShowPathOnTabs.Core.Captioning.TabCaptionBuilder;
+
+namespace ShowPathOnTabs.UnitTests.Captioning;
+
+public class TabCaptionBuilderTests
+{
+    [Fact]
+    public void UniqueFilename_ReturnsUnmodified_Caption()
+    {
+        List<string> tabPaths = [
+            @"C:\Test\Pages\Page.razor",
+            @"C:\Test\Pages\DifferentPage.razor"];
+
+        Assert.Equal("Page.razor", Build(tabPaths.First(), tabPaths));
+    }
+
+    [Fact]
+    public void TwoDuplicateFilenames_ReturnsCaptionWithParentFolder()
+    {
+        List<string> tabPaths = [
+            @"C:\Test\Pages\1\Page.razor",
+            @"C:\Test\Pages\2\Page.razor"];
+
+        Assert.Equal("1/Page.razor", Build(tabPaths.First(), tabPaths));
+    }
+}
