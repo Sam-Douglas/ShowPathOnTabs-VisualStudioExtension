ae4a195b489d17cf834f9d77ed5b301fb20a9397
Sam Douglas <44397135+Sam-Douglas@users.noreply.github.com>
2026-06-25 21:30:15 +0100

    Broke file to test workflow

--- ShowPathOnTabs.UnitTests/Captioning/TabCaptionBuilderTests.cs
+++ ShowPathOnTabs.UnitTests/Captioning/TabCaptionBuilderTests.cs
@@ -2,7 +2,7 @@
 
 namespace ShowPathOnTabs.UnitTests.Captioning;
 
-public class TabCaptionBuilderTests
+public cdlass TabCaptionBuilderTests
 {
     [Fact]
     public void UniqueFilename_ReturnsUnmodified_Caption()
