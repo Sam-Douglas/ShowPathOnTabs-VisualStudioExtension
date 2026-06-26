d75dba9ea7fe1d7a7720a65d19faa68e5377e426
Sam Douglas <44397135+Sam-Douglas@users.noreply.github.com>
2026-06-26 18:01:20 +0100

    Update ShowPathOnTabsPackage.cs

--- ShowPathOnTabs/ShowPathOnTabsPackage.cs
+++ ShowPathOnTabs/ShowPathOnTabsPackage.cs
@@ -25,6 +25,8 @@ namespace ShowPathOnTabs
 
         protected override void Dispose(bool disposing)
         {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
             if (disposing)
             {
                 _tabCaptionService?.Dispose();
